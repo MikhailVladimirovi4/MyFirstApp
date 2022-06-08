ee05a7473819731e90907b5748884d6c6c6bf56f
Mikhail_Bibikov <Mikhail_Bibikov@MYBOX>
2022-06-08 10:48:45 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        byte mineWeight;
+        sbyte speedHorseKnight;
+        ushort castSpeed;
+        short fireDensityShotgan;
+        long swampGrassColor;
+        uint swampGrassDensiry;
+        ulong studentStupidityLevel;
+        int knowledgeHappinessLevel;
+        int tableHeight;
+        bool happiness;
+        char firstSound;
+
+
+        Console.WriteLine("ЯЮниор");
+    }
+}
+
